603888992ef76f94288d4af8d113c11c1d502d88
boodigowda <boodigowda73@gmail.com>
2023-11-09 17:31:43 +0530

    Event-Detail Page Added

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,12 +1,14 @@
 import React from "react";
 import EventListItem from "./event-list-item";
-import classes from './event-list.module.css'
+import classes from "./event-list.module.css";
 
 const EventList = (props) => {
   const { items } = props;
   return (
     <ul className={classes.list}>
-      {items.map(event => <EventListItem key={event.id} event={event} />)}
+      {items?.map((event) => (
+        <EventListItem key={event.id} event={event} />
+      ))}
     </ul>
   );
 };
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,11 +1,29 @@
-import React from 'react'
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+import React, { Fragment } from "react";
 
 const EventDetailPage = () => {
-  return (
-    <div>
-        <h1>EventDetailPage</h1>
-    </div>
-  )
-}
+  const router = useRouter();
 
-export default EventDetailPage
+  const { eventId } = router.query;
+
+  const event = getEventById(eventId);
+
+  if(!event){
+    return <p>No Event Found</p>
+  }
+  return (
+    <Fragment>
+        <EventSummary title={event.title}/>
+        <EventLogistics date={event.date} address={event.location}  image={event.image} imageAlt={event.title}/>
+        <EventContent>
+            <p>{event.description}</p>
+        </EventContent>
+    </Fragment>
+  );
+};
+
+export default EventDetailPage;
