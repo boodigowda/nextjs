42c5e51138e6383ea7999e0b4a0c5e85fe7c6f80
boodigowda <boodigowda73@gmail.com>
2023-11-10 13:26:16 +0530

    All Events Page Added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,12 @@
 import EventList from '@/components/events/event-list'
+import { getAllEvents } from '@/dummy-data'
 import React from 'react'
 
 const AllEventsPAge = () => {
+  const events = getAllEvents()
   return (
     <div>
-        <EventList/>    
+        <EventList items={events}/>
     </div>
   )
 }
